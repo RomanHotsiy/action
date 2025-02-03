a1ba33fabe47748ac3822acb63613afbd695f005
Vova Rutskyi <rutskyi.v@gmail.com>
2025-02-03 16:16:14 +0200

    chore: create a dist dir

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,9 @@ const getIgnoredChangesets = (ignoredList: string | undefined) =>
   ignoredList ? ignoredList.split(",") : [];
 
 const getChangesetCwds = () =>
-  (getOptionalInput("changesetDirectories")?.split(",") || ["."]).map((cwd) =>
-    path.join(process.env.HOME || process.cwd(), cwd)
-  );
+  (getOptionalInput("changesetDirectories") || ".")
+    .split(",")
+    .map((cwd) => path.join(process.env.HOME || process.cwd(), cwd));
 
 (async () => {
   let githubToken = process.env.GITHUB_TOKEN;
